d36d062bc5648eba39fa16a117f5f59113bb716f
Rainbows 4 Blood <rainbows4blood@outlook.com>
2022-02-22 09:28:09 +0100

    Solve Day 3

--- day-3-perfectly-spherical-houses-in-a-vacuum/src/main.rs
+++ day-3-perfectly-spherical-houses-in-a-vacuum/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::Read;
+use crate::CurrentActor::{Robot, Santa};
 
 fn main() {
     let input = parse_input();
@@ -41,7 +42,41 @@ fn solve_santa(input: &Vec<char>) -> usize {
     visited_houses.len()
 }
 
+#[derive(Copy, Clone)]
+enum CurrentActor {
+    Santa,
+    Robot
+}
+
 fn solve_santa_and_robot(input: &Vec<char>) -> usize {
-    let (mut x_santa, y_santa, x_robot, y_robot) = (0, 0, 0, 0);
+    let (mut x_santa, mut y_santa, mut x_robot, mut y_robot) = (0, 0, 0, 0);
     let mut visited_houses: HashSet<(i64, i64)> = HashSet::from([(0, 0)]);
+    let mut current_actor = Santa;
+
+    for direction in input {
+        let (x, y) = match current_actor {
+            Santa =>
+                {
+                    current_actor = Robot;
+                    (&mut x_santa, &mut y_santa)
+                }
+            Robot =>
+                {
+                    current_actor = Santa;
+                    (&mut x_robot, &mut y_robot)
+                }
+        };
+
+        match direction {
+            '<' => *x -= 1,
+            '>' => *x += 1,
+            '^' => *y -= 1,
+            'v' => *y += 1,
+            _ => {}
+        }
+
+        visited_houses.insert((*x, *y));
+    }
+
+    visited_houses.len()
 }
