b44549b1be7f6bf067358e5e95baf9b7d706ace9
Rainbows 4 Blood <rainbows4blood@outlook.com>
2022-02-24 16:47:31 +0100

    Day 6 Solved

--- day-6-probably-a-fire-hazard/src/main.rs
+++ day-6-probably-a-fire-hazard/src/main.rs
@@ -20,8 +20,8 @@ enum InstructionType {
 
 #[derive(Copy, Clone)]
 struct Coordinates {
-    x: i64,
-    y: i64
+    x: usize,
+    y: usize
 }
 
 struct Instruction {
@@ -53,7 +53,7 @@ impl Instruction {
     }
 
     fn parse_coordinate_pair(coordinate_pair: &str) -> Coordinates {
-        let mut coordinates = coordinate_pair.split(",").map(|coordinate| coordinate.parse::<i64>().unwrap());
+        let mut coordinates = coordinate_pair.split(",").map(|coordinate| coordinate.parse::<usize>().unwrap());
         let x = coordinates.nth(0).unwrap();
         let y = coordinates.nth(0).unwrap();
         Coordinates { x, y }
@@ -68,21 +68,29 @@ fn parse_input() -> Vec<Instruction> {
         .collect()
 }
 
-fn solve(input: &Vec<Instruction>) -> (usize, usize) {
+fn solve(input: &Vec<Instruction>) -> (usize, i64) {
     (solve_part_1(input), solve_part_2(input))
 }
 
 fn solve_part_1(input: &Vec<Instruction>) -> usize {
-    let grid: [[bool; 1000]; 1000] = [[false; 1000]; 1000];
+    let mut grid = vec![vec![false; 1000]; 1000];
 
     for instruction in input {
-
+        modify_area_part_1(&mut grid, instruction);
     }
 
-    0
+    grid.iter().map(|row| row.iter().filter(|lamp| **lamp).count()).sum()
 }
 
-fn modify_lamp(lamp: bool, instruction: InstructionType) -> bool {
+fn modify_area_part_1(grid: &mut Vec<Vec<bool>>, instruction: &Instruction) {
+    for x in instruction.start_coordinates.x..=instruction.end_coordinates.x {
+        for y in instruction.start_coordinates.y..=instruction.end_coordinates.y {
+            grid[x][y] = modify_lamp_part_1(grid[x][y], instruction.instruction_type);
+        }
+    }
+}
+
+fn modify_lamp_part_1(lamp: bool, instruction: InstructionType) -> bool {
     match instruction {
         InstructionType::TurnOn => true,
         InstructionType::TurnOff => false,
@@ -90,6 +98,30 @@ fn modify_lamp(lamp: bool, instruction: InstructionType) -> bool {
     }
 }
 
-fn solve_part_2(_input: &Vec<Instruction>) -> usize {
-    0
+
+fn solve_part_2(input: &Vec<Instruction>) -> i64 {
+    let mut grid = vec![vec![0; 1000]; 1000];
+
+    for instruction in input {
+        modify_area_part_2(&mut grid, instruction);
+    }
+
+    grid.iter().map(|row| row.iter().sum::<i64>()).sum()
+}
+
+fn modify_area_part_2(grid: &mut Vec<Vec<i64>>, instruction: &Instruction) {
+    for x in instruction.start_coordinates.x..=instruction.end_coordinates.x {
+        for y in instruction.start_coordinates.y..=instruction.end_coordinates.y {
+            grid[x][y] = modify_lamp_part_2(grid[x][y], instruction.instruction_type);
+        }
+    }
+}
+
+fn modify_lamp_part_2(lamp: i64, instruction: InstructionType) -> i64 {
+    match (lamp, instruction) {
+        (lamp, InstructionType::TurnOn) => lamp + 1,
+        (0, InstructionType::TurnOff) => 0,
+        (lamp, InstructionType::TurnOff) => lamp - 1,
+        (lamp, InstructionType::Toggle) => lamp + 2
+    }
 }
